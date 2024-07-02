57d19dd949c876b68cca96b505869928da6ffb2a
James Blackwell <james.blackwell64@gmail.com>
2024-07-02 02:33:00 +0100

    Lazy loaded plugin

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,9 +1,9 @@
 return {
     'tpope/vim-fugitive',
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-        vim.keymap.set("n", "<leader>gp", ':Git push<CR>');
-        vim.keymap.set("n", "<leader>gca", ':Git commit -am ""<LEFT>');
-        vim.keymap.set("n", "<leader>gcf", ':Git commit % -m ""<LEFT>');
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" });
+        vim.keymap.set("n", "<leader>gp", ':Git push<CR>', { desc = "[G]it [P]ush" });
+        vim.keymap.set("n", "<leader>gca", ':Git commit -am ""<LEFT>', { desc = "[G]it [C]ommit [A]ll" });
+        vim.keymap.set("n", "<leader>gcf", ':Git commit % -m ""<LEFT>', { desc = "[G]it [C]ommit [F]ile" });
     end
 }
