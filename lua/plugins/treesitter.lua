4d5e9debb5e1cf7c8208a7b4385fe87d00e1a4a0
James Blackwell <james.blackwell64@gmail.com>
2024-07-02 02:34:15 +0100

    Lazy loaded treesitter/playground plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
-    'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-    'nvim-treesitter/playground',
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    { 'nvim-treesitter/playground', lazy = true },
     config = function()
         local configs = require('nvim-treesitter.configs')
         configs.setup({
