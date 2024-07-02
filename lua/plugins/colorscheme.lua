05eb99cf5f4f2c17bb0dded8370f0816e4054600
James Blackwell <james.blackwell64@gmail.com>
2024-07-02 02:29:17 +0100

    Make sure color scheme is loaded first

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     'junegunn/seoul256.vim',
     as = 'colors',
+    priority = 1000,
     config = function()
         vim.g.seoul256_background = 233
         vim.cmd('colorscheme seoul256')
