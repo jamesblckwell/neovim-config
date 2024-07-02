12a6518dc568f84b3e857baf1b7d954ef2b60ec3
James Blackwell <james.blackwell64@gmail.com>
2024-07-02 02:33:22 +0100

    Added keymap descriptions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,11 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		vim.keymap.set('n', '<leader>fa', function()
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp" })
+		vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+		vim.keymap.set('n', '<leader>fg', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+		end, { desc = "[F]ind [G]rep" })
 	end
 }
