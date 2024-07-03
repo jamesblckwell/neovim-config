bdec1d385d99cd4227600cd9f683bbb53062ea95
James Blackwell <james.blackwell64@gmail.com>
2024-07-03 18:52:42 +0100

    Lazy loaded fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,9 +1,16 @@
 return {
     'tpope/vim-fugitive',
+    cmd = "Git",
+    keys = {
+        { "<leader>gs",  ":Git<CR>",                  { desc = "[G]it [S]tatus" } },
+        { "<leader>gp",  ":Git push<CR>",             { desc = "[G]it [P]ush" } },
+        { "<leader>gca", ":Git commit -am ''<LEFT>",  { desc = "[G]it [C]ommit [A]ll" } },
+        { "<leader>gcf", ":Git commit % -m ''<LEFT>", { desc = "[G]it [C]ommit [F]ile" } },
+    },
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" });
-        vim.keymap.set("n", "<leader>gp", ':Git push<CR>', { desc = "[G]it [P]ush" });
-        vim.keymap.set("n", "<leader>gca", ':Git commit -am ""<LEFT>', { desc = "[G]it [C]ommit [A]ll" });
-        vim.keymap.set("n", "<leader>gcf", ':Git commit % -m ""<LEFT>', { desc = "[G]it [C]ommit [F]ile" });
+        -- vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" });
+        -- vim.keymap.set("n", "<leader>gp", ':Git push<CR>', { desc = "[G]it [P]ush" });
+        -- vim.keymap.set("n", "<leader>gca", ':Git commit -am ""<LEFT>', { desc = "[G]it [C]ommit [A]ll" });
+        -- vim.keymap.set("n", "<leader>gcf", ':Git commit % -m ""<LEFT>', { desc = "[G]it [C]ommit [F]ile" });
     end
 }
