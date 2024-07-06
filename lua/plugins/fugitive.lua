a7a08590f3e3cc7e83ea703a8d918f7dd2578ef7
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 21:43:39 +0100

    added new fugitive keybindings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,9 +3,11 @@ return {
     cmd = "Git",
     keys = {
         { "<leader>gs",  ":Git<CR>",                  { desc = "[G]it [S]tatus" } },
-        { "<leader>gp",  ":Git! push<CR>",            { desc = "[G]it [P]ush" } },
+        { "<leader>gp",  ":Git push<CR>",             { desc = "[G]it [P]ush" } },
         { "<leader>gca", ":Git commit -am ''<LEFT>",  { desc = "[G]it [C]ommit [A]ll" } },
         { "<leader>gcf", ":Git commit % -m ''<LEFT>", { desc = "[G]it [C]ommit [F]ile" } },
         { "<leader>ga",  ":Git add %<CR>",            { desc = "[G]it [A]dd" } }
+        { "<leader>gb",  ":Git blame %<CR>",          { desc = "[G]it [B]lame" } }
+        { "<leader>gl",  ":Git log<CR>",              { desc = "[G]it [L]og" } }
     }
 }
