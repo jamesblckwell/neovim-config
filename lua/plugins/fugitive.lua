48420d050b87a2825a06dabcee008fb8fec96749
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 21:39:32 +0100

    make gp async

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
     cmd = "Git",
     keys = {
         { "<leader>gs",  ":Git<CR>",                  { desc = "[G]it [S]tatus" } },
-        { "<leader>gp",  ":Git push<CR>",             { desc = "[G]it [P]ush" } },
+        { "<leader>gp",  ":Git! push<CR>",            { desc = "[G]it [P]ush" } },
         { "<leader>gca", ":Git commit -am ''<LEFT>",  { desc = "[G]it [C]ommit [A]ll" } },
         { "<leader>gcf", ":Git commit % -m ''<LEFT>", { desc = "[G]it [C]ommit [F]ile" } },
         { "<leader>ga",  ":Git add %<CR>",            { desc = "[G]it [A]dd" } }
