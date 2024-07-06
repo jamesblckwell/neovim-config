b16fbeec1d950a4256ceab40803136bea2728ae2
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 17:08:42 +0100

    added nvim-ts-context-commentstring for better tsx/jsx handling

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,13 @@
 return {
     'numToStr/Comment.nvim',
+    dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+    },
     config = function()
-        require("Comment").setup()
+        require("Comment").setup({
+            pre_hook = function()
+                return require('ts_context_commentstring.internal').calculate_commentstring()
+            end
+        })
     end
 }
