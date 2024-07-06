d089fb5e0d138411760ab5ef08b054914cb0231f
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 17:11:42 +0100

    Lazy loaded scratch.vim

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -1,7 +1,10 @@
 return {
     'mtth/scratch.vim',
+    cmd = "Scratch",
+    keys = {
+        { "<leader>s", "<cmd>Scratch<cr>", desc = "[S]cratch" },
+    },
     config = function()
         vim.g.scratch_persistence_file = ".scratch"
-        vim.keymap.set('n', '<leader>s', vim.cmd.Scratch);
     end
 }
