c4387f56e927b2dbe45a9b3e68c438bdd83f3945
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 19:50:41 +0100

    added <leader>ga keymap to Git add current file

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,5 +6,6 @@ return {
         { "<leader>gp",  ":Git push<CR>",             { desc = "[G]it [P]ush" } },
         { "<leader>gca", ":Git commit -am ''<LEFT>",  { desc = "[G]it [C]ommit [A]ll" } },
         { "<leader>gcf", ":Git commit % -m ''<LEFT>", { desc = "[G]it [C]ommit [F]ile" } },
+        { "<leader>ga",  ":Git add %<CR>",            { desc = "[G]it [A]dd" } }
     }
 }
