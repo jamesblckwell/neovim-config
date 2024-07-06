05ebcaa37e5584e2405464eb67db2bb06f2889dd
James Blackwell <james.blackwell64@gmail.com>
2024-07-06 17:13:34 +0100

    treesitter config now works 100% of the time, treesitter/playground added back in but lazy loaded

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,17 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    { 'nvim-treesitter/playground', lazy = true },
-    config = function()
-        local configs = require('nvim-treesitter.configs')
-        configs.setup({
-            ensure_installed = { "javascript", "typescript", "c", "lua", "rust" },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                disable = { "html" },
-                additional_vim_regex_hightlighting = true,
-            }
-        })
-    end
+    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    }
 }
