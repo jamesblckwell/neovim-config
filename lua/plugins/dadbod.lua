4a997d242587ab2ac98af75fca71fa165233e20b
James Blackwell <james.blackwell64@gmail.com>
2024-07-07 12:49:23 +0100

    lazy loaded dadbod with key binding

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -4,6 +4,7 @@ return {
         "kristijanhusak/vim-dadbod-ui",
         "kristijanhusak/vim-dadbod-completion"
     },
+    keys = { { "<leader>db", ":DBUIToggle<CR>", { desc = "Toggle [D]ad[B]od" } } },
     config = function()
         return {}
     end
