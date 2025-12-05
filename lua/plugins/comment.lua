c9ab194c51104b20901bce8130eeab5cfcc460db
James Blackwell <james.blackwell64@gmail.com>
2025-12-05 15:25:40 +0100

    test

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,12 @@
 return {
-    'numToStr/Comment.nvim',
-    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
-    keys = { "gcc", "gbc" },
-    config = function()
-        require("Comment").setup({
-            pre_hook = function()
-                return require('ts_context_commentstring.internal').calculate_commentstring()
-            end
-        })
-    end
+	"numToStr/Comment.nvim",
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	keys = { "gcc", "gbc" },
+	config = function()
+		require("Comment").setup({
+			pre_hook = function()
+				return require("ts_context_commentstring.internal").calculate_commentstring()
+			end,
+		})
+	end,
 }
